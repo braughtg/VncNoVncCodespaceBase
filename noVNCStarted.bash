d6de756045f7c65fc760aee61bf694ce9732e395
braughtg <braught@dickinson.edu>
2024-12-02 15:25:17 -0500

    hides the sidebar

--- noVNCStarted.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-CODESPACE_NAME=$(jq -r '.CODESPACE_NAME' < /workspaces/.codespaces/shared/environment-variables.json)
-echo "Codespace name is: $CODESPACE_NAME"
-
-NOVNC_URL="$CODESPACE_NAME-6901.app.github.dev"
-echo "noVNC URL is: $NOVNC_URL"
