ec6d03e626f19b2766b6b8a89e5b207cf2f03780
braughtg <braught@dickinson.edu>
2024-12-02 14:47:13 -0500

    creates no vnc url

--- noVNCStarted.bash
+++ noVNCStarted.bash
@@ -3,3 +3,5 @@
 CODESPACE_NAME=$(jq -r '.CODESPACE_NAME' < /workspaces/.codespaces/shared/environment-variables.json)
 echo "Codespace name is: $CODESPACE_NAME"
 
+NOVNC_URL="$CODESPACE_NAME-6901.app.github.dev"
+echo "noVNC URL is: $NOVNC_URL"
